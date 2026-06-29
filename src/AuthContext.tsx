19df479c724896d1e067c567f8991ab5048510ac
Maheen <mgul18291@gmail.com>
2026-06-28 22:48:00 -0400

    updated redirect for local dev

--- src/AuthContext.tsx
+++ src/AuthContext.tsx
@@ -57,7 +57,7 @@ export function AuthContextProvider({ children }: any) {
     // handle password reset here
     const resetPassword = async (email: string) => {
         const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-            redirectTo: "http://flashiercards.pages.dev/resetPassword",
+            redirectTo: "http://localhost:5173/resetPassword",
         });
 
         if (error) {
