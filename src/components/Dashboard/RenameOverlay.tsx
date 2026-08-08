ab7943cd9748a62ecfc7d439eaddcafabbc002c5
Maheen <mgul18291@gmail.com>
2026-08-08 14:40:11 -0400

    moved file to dashboard

--- src/components/Dashboard/RenameOverlay.tsx
+++ src/components/Dashboard/RenameOverlay.tsx
@@ -1,4 +1,4 @@
-import styles from "../styles/Dashboard.module.css";
+import styles from "../../styles/Dashboard.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 
